954f6419203f96da61edce32809a7f2ab0fb42c2
Megha_Chavan <chavanmegha665@gmail.com>
2026-07-24 19:50:39 +0530

    Changes in Student management APP

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -7,8 +7,11 @@ fun main() {
    while(true){
       println("\n----------student Management System---------")
       println("1. Add Student")
-      println("2. View Students")
-      println("3. Exit")
+      println("2. View Student")
+      println("3. Update Student")
+      println("4. Delete Students")
+      println("5. Search Students")
+      println("6. Exit")
       println("Enter your choice: ")
       val choice = readLine()!!.toInt()
       when(choice){
@@ -28,7 +31,28 @@ fun main() {
             manager.viewStudents()
          }
          3 -> {
-            println("Thank you")
+           print("Enter Student Id to update:")
+            val id = readLine()!!.toInt()
+            print("Enter Student Name:")
+            val name = readLine()!!
+            print("Enter Student Age:")
+            val age = readLine()!!.toInt()
+            print("Enter Student Course:")
+            val course = readLine()!!
+            manager.updateStudent(id, name, age, course)
+         }
+         4 -> {
+            print("Enter Id to Delete:")
+            val id = readLine()!!.toInt()
+            manager.deleteStudent(id)
+         }
+         5 -> {
+            print("Enter Student Id to Find:")
+            val id = readLine()!!.toInt()
+            manager.searchStudent(id)
+         }
+         6 -> {
+            println("Thank You")
             break
          }
          else -> {
--- src/main/kotlin/StudentManager.kt
+++ src/main/kotlin/StudentManager.kt
@@ -16,7 +16,29 @@ class StudentManager{
             println(student)
         }
     }
-    fun searchStudents(id: Int){
+    fun updateStudent(id: Int, name: String, age: Int, course: String){
+        val student = students.find { it.id == id }
+        if(student != null){
+            student.name = name
+            student.age = age
+            student.course = course
+            println("Student updated successfully!")
+        }
+        else{
+            println("Student not found")
+        }
+    }
+    fun deleteStudent(id: Int){
+        val student = students.find { it.id == id }
+        if(student != null){
+            students.remove(student)
+            println("Student deleted successfully!")
+        }
+        else{
+            println("Student not found")
+        }
+    }
+    fun searchStudent(id: Int){
         val student = students.find { it.id == id }
         if(student != null){
             println("Student found")
